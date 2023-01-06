8170c7444bde4e9e51f5a8c892144726efff72f5
pgarman608 <cnsg2198@mail.com>
2023-01-06 20:24:39 +0100

    25%

--- controladores/index.js
+++ controladores/index.js
@@ -11,21 +11,46 @@ let botonInferior = botones[3]
 let bgSuperior = background[0]
 let bgIntermedio = background[1]
 let bgInferior = background[2]
-botonSuperior.addEventListener("click",async function(evt) {
+//Contenedores de informacion
+let cont1 = document.getElementById("contenedor1")
+let frame1 = document.getElementById("inicioframe")
+botonSuperior.onclick = async function() {
+    botonSuperior.classList.add("botonout")
+    frame1.classList.add("antvoff")
+    await sleep(650)
+    botonSuperior.classList.remove("botonout")
+    botonSuperior.classList.remove("btnVisible")
+    frame1.classList.remove("antvoff")
+    
+    cont1.style.visibility = "hidden"
     botonSuperior.style.visibility = "hidden"
     let tamanioSuperior = 78
     let tamaniointermedio = 11
     for (let index = 0; index < 67; index = index + 0.5) {
         tamanioSuperior = tamanioSuperior- 0.5
         tamaniointermedio = tamaniointermedio + 0.5
+        cont1.style.height = tamanioSuperior+"vh";
         bgSuperior.style.height = tamanioSuperior+"vh"
         bgIntermedio.style.height = tamaniointermedio+"vh"
         await sleep(6)
     }
+    botonSuperior.style.visibility = "hidden"
+    cont1.style.visibility = "hidden"
     botonIntermedioUno.style.visibility = "visible"
     botonIntermedioDos.style.visibility = "visible"
-})
+    botonIntermedioUno.classList.add("botonin")
+    botonIntermedioDos.classList.add("botonin")
+    await sleep(710)
+    botonIntermedioUno.classList.remove("botonin")
+    botonIntermedioDos.classList.remove("botonin")
+    
+}
 botonIntermedioUno.addEventListener("click",async function(evt) {
+    botonIntermedioUno.classList.add("botonout")
+    botonIntermedioDos.classList.add("botonout")
+    await sleep(300)
+    botonIntermedioUno.classList.remove("botonout")
+    botonIntermedioDos.classList.remove("botonout")
     botonIntermedioUno.style.visibility = "hidden"
     botonIntermedioDos.style.visibility = "hidden"
     let tamanioIntermedio = 78
@@ -33,13 +58,26 @@ botonIntermedioUno.addEventListener("click",async function(evt) {
     for (let index = 0; index < 67; index = index + 0.5) {
         tamanioSuperior = tamanioSuperior + 0.5
         tamanioIntermedio = tamanioIntermedio - 0.5
+        cont1.style.height = tamanioSuperior+"vh";
         bgSuperior.style.height = tamanioSuperior+"vh"
         bgIntermedio.style.height = tamanioIntermedio+"vh"
         await sleep(6)
     }
-    botonSuperior.style.visibility = "visible"    
+    botonSuperior.style.visibility = "visible"
+    cont1.style.visibility = "visible"
+    botonSuperior.classList.add("botonin")
+    frame1.classList.add("antvon")
+    await sleep(650)
+    frame1.classList.remove("antvon")
+    botonSuperior.classList.remove("botonin")
+    
 })
 botonIntermedioDos.addEventListener("click",async function(evt) {
+    botonIntermedioUno.classList.add("botonout")
+    botonIntermedioDos.classList.add("botonout")
+    await sleep(220)
+    botonIntermedioUno.classList.remove("botonout")
+    botonIntermedioDos.classList.remove("botonout")
     botonIntermedioUno.style.visibility = "hidden"
     botonIntermedioDos.style.visibility = "hidden"
     let tamanioIntermedio = 78
@@ -51,10 +89,15 @@ botonIntermedioDos.addEventListener("click",async function(evt) {
         bgIntermedio.style.height = tamanioIntermedio+"vh"
         await sleep(6)
     }
-    botonSuperior
     botonInferior.style.visibility = "visible"
+    botonInferior.classList.add("botonin")
+    await sleep(710)
+    botonInferior.classList.remove("botonin")
 })
 botonInferior.addEventListener("click",async function(evt) {
+    botonInferior.classList.add("botonout")
+    await sleep(220)
+    botonInferior.classList.remove("botonout")
     botonInferior.style.visibility = "hidden"
     let tamanioInferior = 78
     let tamanioIntermedio = 11
@@ -65,6 +108,11 @@ botonInferior.addEventListener("click",async function(evt) {
         bgInferior.style.height = tamanioInferior+"vh"
         await sleep(6)
     }
+    botonIntermedioUno.classList.add("botonin")
+    botonIntermedioDos.classList.add("botonin")
+    await sleep(220)
+    botonIntermedioUno.classList.remove("botonin")
+    botonIntermedioDos.classList.remove("botonin")
     botonIntermedioUno.style.visibility = "visible"
     botonIntermedioDos.style.visibility = "visible"
 })
